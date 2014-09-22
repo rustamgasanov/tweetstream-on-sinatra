0a6bafa55cd84ecd1d7991e6c160ccde10eb858c
Rustam Gasanov <rustamagasanov@gmail.com>
2014-09-22 15:46:13 +0400

    [+] added ENV variables if config isn't found

--- app.rb
+++ app.rb
@@ -4,7 +4,12 @@ require 'json'
 require 'yaml'
 require './twitter_streamer'
 
-twitter_keys = YAML.load_file('config.yml')
+begin
+  twitter_keys = YAML.load_file('config.yml')
+rescue
+  twitter_keys = { 'consumer_key' => ENV['consumer_key'], 'consumer_secret' => ENV['consumer_secret'],
+    'oauth_token' => ENV['oauth_token'], 'oauth_secret' => ENV['oauth_secret'] }
+end
 params = { :track => 'coffee' }
 # params = { :follow => '254540449' }
 
